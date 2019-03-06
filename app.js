c00d2febbc08a8836eaa2cb64586b7ee4839b8a7
Micah Monfrini <ffrock905@gmail.com>
2019-03-05 20:15:40 -0600

    add comments

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ document.getElementById('email').addEventListener('blur', validateEmail);
 document.getElementById('phone').addEventListener('blur', validatePhone);
 
 function validateName() {
+  // grab element
   const name = document.getElementById('name');
+  // regular expression (letters between 2 and 10 characters)
   const re = /^[a-zA-Z]{2,10}$/
 
+  // if the value in the form does not match re parameters
   if(!re.test(name.value)) {
+    // add is-invalid class to the element
     name.classList.add('is-invalid');
+    // if it matches, make sure is-invalid class is removed
   } else {
     name.classList.remove('is-invalid');
   }
